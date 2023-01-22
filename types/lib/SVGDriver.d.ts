bc0cf78f098b8fb6a4e850b240789a6b7a4c65c8
Joe Hildebrand <joe-github@cursive.net>
2023-01-21 18:04:14 -0700

    Move to xmlbuilder2.  Fixes #33.

--- types/lib/SVGDriver.d.ts
+++ types/lib/SVGDriver.d.ts
@@ -1,13 +1,22 @@
 export default class SVGDriver extends Driver {
     path_count: number;
-    doc: xml.XMLElement;
-    /** @type {xml.XMLElement} */
-    top: xml.XMLElement;
+    /**
+     * @type {Builder}
+     */
+    doc2: Builder;
+    /**
+     * @type {Builder}
+     */
+    root: Builder;
+    /**
+     * @type {Builder}
+     */
+    top: Builder;
     /**
      * @param {string} [name]
-     * @returns {xml.XMLElement}
+     * @returns {Builder}
      */
-    draw_group(name?: string): xml.XMLElement;
+    draw_group(name?: string): Builder;
     /**
      * @param {string|(string|Point)[]} cmds
      * @param {string} klasses Space-separated CSS classes
@@ -15,6 +24,6 @@ export default class SVGDriver extends Driver {
      */
     draw_path(cmds: string | (string | Point)[], klasses: string): any;
 }
+export type Builder = import('xmlbuilder2/lib/interfaces').XMLBuilder;
 import Driver from "./driver.js";
-import xml from "xmlbuilder";
 import { Point } from "./point.js";
