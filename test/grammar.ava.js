80eec51252e27ad7e033b5e384da24f7f3903ca8
Joe Hildebrand <joe-github@cursive.net>
2023-04-16 16:07:35 -0600

    Add a little parser coverage

--- test/grammar.ava.js
+++ test/grammar.ava.js
@@ -6,6 +6,8 @@ test('parse error', t => {
   t.throws(() => parse('Bob'))
   t.throws(() => parse('Bob -'))
   t.throws(() => parse('Bob ->'))
+  t.throws(() => parse('Bob - >'))
+  t.throws(() => parse('Bob a'))
   t.throws(() => parse('advance'))
   t.throws(() => parse('advance '))
   t.throws(() => parse('advance\t'))
@@ -27,6 +29,8 @@ test('participant', t => {
   // TODO: this ought to work.
   // t.truthy(parse('participant Bob cob\n'));
 
+  t.throws(() => parse('participant'))
+  t.throws(() => parse('participantBob'))
   t.throws(() => parse('participant "Bob'))
   t.throws(() => parse('participant "Bob"'))
   t.throws(() => parse('participant "Bob" '))
@@ -37,13 +41,46 @@ test('participant', t => {
 test('send', t => {
   // Here: Bob -> Alice: Test duration=2[duration=2]
   t.throws(() => parse('Bob '))
+  t.throws(() => parse('Bob@'))
   t.throws(() => parse('Bob @'))
   t.throws(() => parse('Bob @ '))
   t.throws(() => parse('Bob @t'))
   t.throws(() => parse('Bob @t ->'))
   t.throws(() => parse('t:'))
+  t.throws(() => parse('Bob -> Alice\nBob'))
 })
 
-test('opt', t => {
+test('blocks', t => {
   t.throws(() => parse('opt'))
+  t.throws(() => parse('loop'))
+  t.throws(() => parse('block'))
+  t.throws(() => parse('opt '))
+  t.throws(() => parse('loop '))
+  t.throws(() => parse('block '))
+  t.throws(() => parse('opt A'))
+  t.throws(() => parse('loop A'))
+  t.throws(() => parse('block "'))
+  t.throws(() => parse('opt "'))
+  t.throws(() => parse('loop "'))
+  t.throws(() => parse('block "'))
+})
+
+test('options', t => {
+  t.truthy(parse('A->B [ duration = 2 ,\tadvance=""\t]'))
+})
+
+test('note', t => {
+  t.throws(() => parse('note'))
+  t.throws(() => parse('note '))
+  t.throws(() => parse('noteBob'))
+  t.throws(() => parse('note Bob'))
+  t.truthy(parse('note Bob: #'))
+  t.truthy(parse('note Bob: \\\\ #'))
+})
+
+test('set', t => {
+  t.throws(() => parse('set'))
+  t.throws(() => parse('set '))
+  t.throws(() => parse('set UNKNOWN_PROP'))
+  t.throws(() => parse('set no_feet "'))
 })
